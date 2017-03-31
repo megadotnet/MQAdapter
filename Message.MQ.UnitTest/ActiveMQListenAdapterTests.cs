cdc21fa78d1f62e3c7c4e25a831e0b139017fbd4
Megadotnet <wintersunair@gmail.com>
2017-03-31 22:10:17 +0800

    Added ActiveMQListenAdapterTests

---
+++ Message.MQ.UnitTest/ActiveMQListenAdapterTests.cs
@@ -0,0 +1,27 @@
+﻿using BusniessEntities.Models;
+using Messag.Utility.Config;
+using Megadotnet.MessageMQ.Adapter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Xunit;
+
+namespace Megadotnet.Message.MQ.UnitTest
+{
+    /// <summary>
+    /// ActiveMQListenAdapterTests
+    /// </summary>
+    public class ActiveMQListenAdapterTests
+    {
+        /// <summary>
+        /// Shoulds the send message for model to mq.
+        /// </summary>
+        [Fact]
+        public void ShouldSendMessageForModelToMQ()
+        {
+
+        }
+    }
+}
