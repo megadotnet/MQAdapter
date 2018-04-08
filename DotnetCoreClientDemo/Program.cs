1f92fee005a45169240bd9bd39ba20a54bc8f223
megadotnet <wintersunair@gmail.com>
2018-04-08 09:55:57 +0800

    Updated correct package reference

--- DotnetCoreClientDemo/Program.cs
+++ DotnetCoreClientDemo/Program.cs
@@ -14,10 +14,11 @@ namespace DotnetCoreClientDemo
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Prepare Connect ");
+            Console.WriteLine("Prepare Connect MQ");
 
             var messageManager = new MessageManager();
 
+            Console.WriteLine("Prepare Send message to MQ");
             if (messageManager.SendOneMessage())
             {
                 Console.WriteLine("Done, press Any Key will be close");
